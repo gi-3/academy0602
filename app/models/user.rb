6c0317e2cdf74c1b0cad8f0c13fc81c4519a3b89
Takagi <takaagiii@sonicgarden.jp>
2015-06-02 14:44:45 +0900

    Devise/name追加

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ActiveRecord::Base
+  validates :name, presence: true
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
